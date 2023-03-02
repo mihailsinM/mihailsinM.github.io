2c4c22ac57e5c7e3ed782614b18ef8c20cbfdf70
mihailsinM <mihailsin49@gmail.com>
2023-03-02 22:29:33 +0200

    jsx

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>my app</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// const elem = <h2>Hello World!</h2>;
+// const elem = React.createElement("h2", { className: 'greetings' }, 'Hello World!');
+
+
+const text = 'Hello World!';
+
+const elem = (
+  <div>
+    <h2>Text: {text}</h2>
+    <input type='text' />
+    <label htmlFor="" className="htmlFor"></label>
+    <button>Click me!</button>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+  elem
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
