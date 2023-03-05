5f1817ed82b5742b7a47e5eca660fabc281579a2
mihailsinM <mihailsin49@gmail.com>
2023-03-05 13:43:04 +0200

    properties components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Component, StrictMode } from 'react';
+// import { Component, StrictMode } from 'react';
 import './App.css';
 
-const Header = () => {
-  return <h2>Hello world!</h2>
-}
+// const Header = () => {
+//   return <h2>Hello world!</h2>
+// }
 
 // const Field = () => {
 //   const holder = 'Enter here';
@@ -17,36 +17,53 @@ const Header = () => {
 // }
 
 
-class Field extends Component {
-  render() {
-    const holder = 'Enter here';
-    const styleField = {
-      width: '300px'
-    };
-    return <input
-      placeholder={holder}
-      type="text"
-      style={styleField} />
-  }
-}
+// class Field extends Component {
+//   render() {
+//     const holder = 'Enter here';
+//     const styleField = {
+//       width: '300px'
+//     };
+//     return <input
+//       placeholder={holder}
+//       type="text"
+//       style={styleField} />
+//   }
+// }
 
-function Btn() {
-  const text = 'Login.';
-  const logged = true;
+// function Btn() {
+//   const text = 'Login.';
+//   const logged = true;
 
-  return <button>{logged ? 'Enter' : text}</button>
+//   return <button>{logged ? 'Enter' : text}</button>
+// }
+
+// function WhoAmI(props) {
+function WhoAmI({ name, surname, link }) {
+  return (
+    <div>
+      {/* <h1>Wy name is {props.name}, surname - {props.surname}</h1>
+      <a href={props.link}>My profile</a> */}
+
+      {/* <h1>Wy name is {name.firstName}, surname - {surname}</h1> */}
+      <h1>Wy name is {name.firstName}, surname - {surname()}</h1>
+      <a href={link}>My profile</a>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <StrictMode>
+      <WhoAmI name={{ firstName: 'John' }} surname={()=>{return "Smith"}} link='https://github.com/mihailsinM' />
+      {/* <WhoAmI name={{ firstName: 'Alex' }} surname='Smith' link='https://github.com/mihailsinM' /> */}
+      {/* <WhoAmI name={{ firstName: 'Miha' }} surname='Smith' link='https://github.com/mihailsinM' /> */}
+      {/* <StrictMode>
         <Header />
       </StrictMode>
       <Field />
-      <Btn />
+      <Btn /> */}
     </div>
   );
 }
-export { Header };
+// export { Header };
 export default App;
