73d0fdc3c30dbe3f4c9e5e68976cb2a2a5f15bb3
mihailsinM <mihailsin49@gmail.com>
2023-04-12 00:58:10 +0300

    added React Styled Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, .2);
+  a {
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${props => props.active ? "orange" : 'black'};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2`
+  font-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0, 0, 0, .2);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, .2);
+`;
 
 class WhoAmI extends Component {
   // eslint-disable-next-line no-useless-constructor
@@ -14,7 +43,9 @@ class WhoAmI extends Component {
 
   }
   nextYear = () => {
-    this.setState(state => ({ years: state.years + 1 }))
+    this.setState(state => ({
+      years: state.years + 1
+    }))
   }
 
   commitInputChanges = (e, color) => {
@@ -31,30 +62,35 @@ class WhoAmI extends Component {
     console.log(this);
 
     return (
-      <>
-        <button onClick={this.nextYear}>{this.state.text}</button>
-        <h1>My name is {name},
+      <EmpItem active>
+        <Button onClick={this.nextYear}>{this.state.text}</Button>
+        <Header>My name is {name},
           surname - {surname},
           age - {years},
-          position - {position}</h1>
+          position - {position}</Header>
         <a href={link}>My profile</a>
         <form>
           <span>Enter job title.</span>
           {/* <input type='text' onInput={this.commitInputChanges} /> */}
           <input type='text' onChange={(e) => this.commitInputChanges(e, 'some color')} />
         </form>
-      </ >
+      </EmpItem >
     )
   }
 
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <WhoAmI name='John' surname="Smith" link='https://github.com/mihailsinM' />
       <WhoAmI name='Alex' surname="Smith" link='https://github.com/mihailsinM' />
-    </div>
+    </Wrapper>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Header } from './App';
+import { Button } from './App';
+import styled from 'styled-components';
+
+const BigButtom = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+  text-align: center;
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <BigButtom as='a'>Send a report.</BigButtom>
+    {/* <BigButtom> +++ </BigButtom> */}
+    {/* <Button /> */}
   </React.StrictMode>
 
 );
